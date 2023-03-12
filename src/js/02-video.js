dfe29cba7085cda1eb565ad41a1323399293d438
Iryna0810 <irinavakulenko2000@gmail.com>
2023-03-12 21:42:16 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from "@vimeo/player";
+import { throttle } from "lodash";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const throttledFunction = throttle(videoPlayerCurrentTime, 1000)
+
+player.on('timeupdate', throttledFunction);
+
+function videoPlayerCurrentTime ({ duration, percent, seconds }) {
+    // console.log('played the video!');
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+
+
+
 
