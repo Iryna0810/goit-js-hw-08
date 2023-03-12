eab3169d84adcbd8233cb488951e382853111f79
Iryna0810 <irinavakulenko2000@gmail.com>
2023-03-12 22:25:28 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import { throttle } from "lodash";
 
+const emailInput = document.querySelector('.feedback-form input[type="email"]');
+console.log(emailInput);
+
+const messageInput = document.querySelector('.feedback-form textarea');
+console.log(messageInput);
+
+let feedbackFormState = {
+    email: localStorage.getItem.email,
+    message: localStorage.getItem.message
+};
+
+emailInput.addEventListener("input", (event) => {
+    feedbackFormState.email = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState))
+});
+
+messageInput.addEventListener("input", (event) => {
+    feedbackFormState.message = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState))
+});
